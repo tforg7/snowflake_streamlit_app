a0493fcc80a4cb5407f6675997ad381a09e922a1
Thomas Forget <forget.th@gmail.com>
2022-08-12 14:34:17 +0200

    Update snowflake_app.py
    Add some Emojis and extra text

--- snowflake_app.py
+++ snowflake_app.py
@@ -3,6 +3,7 @@ import streamlit as s
 
 s.title('My parents New Healty dinner')
 s.header('Breakfast Menu')
-s.text('Omega3 & Blueberry Oatmeal')
-s.text('Kale, Spinach & Rocket Smoothie')
-s.text('Hard-boiled Free-Range Egg')
+s.text('🥣 Omega3 & Blueberry Oatmeal')
+s.text('🥗 Kale, Spinach & Rocket Smoothie')
+s.text('🐔 Hard-boiled Free-Range Egg')
+s.text('🥑🍞 Avocado Toast')
