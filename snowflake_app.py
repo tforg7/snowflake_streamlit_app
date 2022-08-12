c85768c95eca24c7d6fd372a64732bf469c03661
Thomas Forget <forget.th@gmail.com>
2022-08-12 14:40:19 +0200

    Update snowflake_app.py
    Add multiselect tab

--- snowflake_app.py
+++ snowflake_app.py
@@ -2,7 +2,9 @@
 import streamlit as s
 import pandas as pd
 
+# Import CSV and chage index
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 s.title('My parents New Healty dinner')
 s.header('Breakfast Menu')
@@ -14,3 +16,7 @@ s.text('🥑🍞 Avocado Toast')
 
 s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 s.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+s.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
