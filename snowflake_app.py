27936538ea52234255fa139aeaddc4ff73a9f9af
Thomas Forget <forget.th@gmail.com>
2022-08-12 14:31:08 +0200

    Update snowflake_app.py

--- snowflake_app.py
+++ snowflake_app.py
@@ -2,3 +2,7 @@
 import streamlit as s
 
 s.title('My parents New Healty dinner')
+s.header('Breakfast Menu')
+s.text('Omega3 & Blueberry Oatmeal')
+s.text('Kale, Spinach & Rocket Smoothie')
+s.text('Hard-boiled Free-Range Egg')
