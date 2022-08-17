cbceb870d0e74e051d179ba676bafce82245bdfe
Thomas Forget <forget.th@gmail.com>
2022-08-17 10:06:03 +0200

    Update snowflake_app.py
    New import

--- snowflake_app.py
+++ snowflake_app.py
@@ -2,6 +2,7 @@
 import streamlit as s
 import pandas as pd
 import requests
+import snowflake.connector as sfc
 
 # Import CSV and chage index
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
